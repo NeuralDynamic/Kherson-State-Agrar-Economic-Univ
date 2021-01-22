97d91ea87f9055e4fa543c3d424b59143a5f5156
DiablosWhisper <shelyakingleb17@gmail.com>
2021-01-22 17:04:58 +0200

    update: signals were added

--- website/university/signals.py
+++ website/university/signals.py
@@ -4,5 +4,33 @@ from django.dispatch import receiver
 #endregion
 
 #region             -----Internal Imports-----
-from .models import 
+from .models import (Staff, Faculty, Cathedra)
+#endregion
+
+
+#region               -----Subdivisions-----
+@receiver(post_delete, sender=Cathedra)
+@receiver(post_delete, sender=Faculty)
+def delete_on_delete(instance: object,
+**kwargs)->None:
+    """
+    Deletes related gallery of object\n
+    :param instance: object instance\n
+    @return None
+    """
+    (instance.gallery.delete() if
+    instance.gallery else None)
+#endregion
+
+#region                  -----Staff-----
+@receiver(post_delete, sender=Staff)
+def delete_on_delete(instance: Staff,
+**kwargs)->None:
+    """
+    Deletes related library of staff\n
+    :param instance: staff instance\n
+    @return None
+    """
+    (instance.library.delete() if
+    instance.library else None)
 #endregion
