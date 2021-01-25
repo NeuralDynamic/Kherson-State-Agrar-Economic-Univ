d3c7d6f6eec7677a8f6d65859425d3fdb6cf5003
DiablosWhisper <shelyakingleb17@gmail.com>
2021-01-25 16:20:59 +0200

    update: subdomen

--- website/website/settings/prod.py
+++ website/website/settings/prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'Au+Sr,t>U]<8\Zn8Z3X!uTq<Q#4<sQS^Ezda5DhkV}C)&Nu{Sjy@Hq#y`nM2'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['ksau.gold-rain.club']
+ALLOWED_HOSTS = ['ksau.neuraldynamics.online']
 
 DATABASES = {
     'default': {
