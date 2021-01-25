95509560fc962eba8b806c5ebbdb8a1a0c1dded9
Nickolay Krykunov <nik.krikunov.99@gmail.com>
2021-01-25 15:05:28 +0200

    update layouts

--- website/website/settings/prod.py
+++ website/website/settings/prod.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+
 from .django import *
 from .project import *
 from .third_party import *
+
+load_dotenv()
 
 SECRET_KEY = 'Au+Sr,t>U]<8\Zn8Z3X!uTq<Q#4<sQS^Ezda5DhkV}C)&Nu{Sjy@Hq#y`nM2'
 
@@ -10,14 +14,13 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 DATABASES = {
     'default': {
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'ENGINE': 'django.db.backends.sqlite3',
-        'HOST': os.environ['DATABASE_HOST'],
-        'NAME': os.environ['DATABASE_NAME'],
-        'PORT': os.environ['DATABASE_PORT'],
-        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'ENGINE': os.environ.get('ENGINE')
     }
 }
-
 
 print(">>> START PROJECT WITH PROD SETTINGS <<<")
