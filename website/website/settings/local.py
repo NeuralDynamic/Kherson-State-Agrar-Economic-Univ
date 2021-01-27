e1f0eaff06360608674b20318808153205385706
DiablosWhisper <shelyakingleb17@gmail.com>
2021-01-27 19:19:03 +0200

    update: ip fixes

--- website/website/settings/local.py
+++ website/website/settings/local.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'rt^p8v+8e*gyo4!)qx&4gkb^n5o1w6fc-$bvko9-=5slu225bv'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['ksau.neuraldynamics.online']
 
 DATABASES = {
     'default': {
