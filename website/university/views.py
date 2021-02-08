a5e6e69db50bee9c9340ac2634e83396ca2bd4a1
DiablosWhisper <shelyakingleb17@gmail.com>
2021-02-08 09:30:11 +0200

    update: views fixes

--- website/university/views.py
+++ website/university/views.py
@@ -9,27 +9,22 @@ from .services.service import StaffService, FacultyService, CathedarService
 #region				   -----Type Hints-----
 #endregion
 
-#region				   -----Teacher views-----
-
+#region				 -----Teacher views-----
 def teacher_view(request, teacher_id):
     context = StaffService().get_staff(pk=teacher_id)
     return render(request,'university/teacher.html',
                         context=context)
-
 #endregion
 
 
-#region				   -----Faculty views-----
-
+#region				 -----Faculty views-----
 def faculty_view(request, faculty_id):
     context = FacultyService().get_faculty(faculty_id)
     return render(request,'university/faculty.html',context=context)
-
 #endregion
 
 
-#region				   -----Faculty views-----
-
+#region				 -----Faculty views-----
 def cathedra_view(request, cathedra_id):
     context = CathedarService().get_cathedra(pk=cathedra_id)
     return render(request,'university/cathedra.html',context=context)
