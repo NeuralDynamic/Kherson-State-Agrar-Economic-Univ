03b943eb6351b1b6ec9909608399e66a808ab103
DiablosWhisper <shelyakingleb17@gmail.com>
2021-02-16 20:01:11 +0200

    create: main page

--- website/content/cms_plugins.py
+++ website/content/cms_plugins.py
@@ -9,7 +9,43 @@ from django.core.files.images import get_image_dimensions
 from django.utils.translation import ugettext as _
 #endregion
 
+#region				-----Banner plugins-----
+class Info1Plugin(CMSPluginBase):
+    render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__banner.html"
+    module=_("Banner plugins")
+    model=cms_models.Banner
+    name=_("Banner")
+#endregion
 
+#region				-----Cards plugins-----
+class Info1Plugin(CMSPluginBase):
+    render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__cards.html"
+    module=_("Cards plugins")
+    model=cms_models.Cards
+    name=_("Cards")
+#endregion
+
+#region				-----Infos plugins-----
+class Info1Plugin(CMSPluginBase):
+    render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__info.html"
+    module=_("Info1 plugins")
+    model=cms_models.Info1
+    name=_("Info1")
+
+class Info2Plugin(CMSPluginBase):
+    render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__info.html"
+    module=_("Info2 plugins")
+    model=cms_models.Info2
+    name=_("Info2")
+#endregion
+
+#region				-----News plugins-----
+class NewsPlugin(CMSPluginBase):
+    render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__news.html"
+    module=_("News plugins")
+    model=cms_models.News
+    name=_("News")
+#endregion
 
 #region				-----Utils plugins-----
 class ExternalLinkPlugin(CMSPluginBase):
@@ -27,9 +63,7 @@ class ExternalLinkPlugin(CMSPluginBase):
     #endregion
 #endregion
 
-
 #region				-----Pages plugins-----
-
 class FooterBlock(CMSPluginBase):
     #region           -----Information-----
     render_template=settings.TEMPLATE_DIR+"/cms-plugins/footer.html"
