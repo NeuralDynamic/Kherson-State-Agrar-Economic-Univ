c49251ff175533f078a2b754ab3e138338d68ffc
DiablosWhisper <shelyakingleb17@gmail.com>
2021-02-17 14:53:29 +0200

    update: cms fixes

--- website/content/cms_plugins.py
+++ website/content/cms_plugins.py
@@ -7,22 +7,36 @@ from cms.plugin_base import CMSPluginBase
 from django.conf import settings
 from django.core.files.images import get_image_dimensions
 from django.utils.translation import ugettext as _
+
+from news.models import Paper
 #endregion
 
 #region				-----Banner plugins-----
-class Info1Plugin(CMSPluginBase):
+class BannerPlugin(CMSPluginBase):
     render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__banner.html"
     module=_("Banner plugins")
     model=cms_models.Banner
     name=_("Banner")
+
+    #region            -----Rendering-----
+    def render(self, context, instance, placeholder):
+        context.update({"instance": instance})
+        return context
+    #endregion
 #endregion
 
 #region				-----Cards plugins-----
-class Info1Plugin(CMSPluginBase):
+class CardsPlugin(CMSPluginBase):
     render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__cards.html"
     module=_("Cards plugins")
     model=cms_models.Cards
     name=_("Cards")
+
+    #region            -----Rendering-----
+    def render(self, context, instance, placeholder):
+        context.update({"instance": instance})
+        return context
+    #endregion
 #endregion
 
 #region				-----Infos plugins-----
@@ -32,11 +46,23 @@ class Info1Plugin(CMSPluginBase):
     model=cms_models.Info1
     name=_("Info1")
 
+    #region            -----Rendering-----
+    def render(self, context, instance, placeholder):
+        context.update({"instance": instance})
+        return context
+    #endregion
+
 class Info2Plugin(CMSPluginBase):
     render_template=settings.TEMPLATE_DIR+"/cms-plugins/home__info.html"
     module=_("Info2 plugins")
     model=cms_models.Info2
     name=_("Info2")
+
+    #region            -----Rendering-----
+    def render(self, context, instance, placeholder):
+        context.update({"instance": instance})
+        return context
+    #endregion
 #endregion
 
 #region				-----News plugins-----
@@ -45,6 +71,13 @@ class NewsPlugin(CMSPluginBase):
     module=_("News plugins")
     model=cms_models.News
     name=_("News")
+
+    #region            -----Rendering-----
+    def render(self, context, instance, placeholder):
+        context.update({"articles": Paper.objects.all()[:5]})
+        context.update({"instance": instance})
+        return context
+    #endregion
 #endregion
 
 #region				-----Utils plugins-----
