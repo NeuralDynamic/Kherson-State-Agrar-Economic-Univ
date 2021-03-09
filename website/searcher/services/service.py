112af3394ba4e9055bd0f27e03bb2930ffa85b06
Nickolay Krykunov <nik.krikunov.99@gmail.com>
2021-03-09 15:33:30 +0200

    fix search

--- website/searcher/services/service.py
+++ website/searcher/services/service.py
@@ -1,11 +1,13 @@
 #region				-----External Imports-----
 from django.db.models import Q
+from django.utils.translation import get_language
 from functools import reduce
 from operator import or_
 from typing import List
 #endregion
 
 #region				-----Internal Imports-----
+from cms.models import Page, Title
 from gallery.models import (Gallery, Image)
 from library.models import Book
 from news.models import (NewsFeed, Paper)
@@ -47,5 +49,14 @@ class SearchService(object):
             if search_result:
                 result.search_result=True
                 setattr(result, model.__name__.lower(),search_result)
-        print(result.__dict__)
+        # search by cms pages
+        cms_slugs=[]
+        current_language=get_language()
+        cms_pages = Page.objects.filter(publisher_is_draft=False).all()
+        for cms_page in cms_pages:
+            page_title_obj=Title.objects.filter(page_id=cms_page.pk, 
+                                            language=current_language).first()
+            if page_title_obj and query in page_title_obj.title:
+                cms_slugs.append(page_title_obj.slug)
+        if cms_slugs: result.pages_slugs=cms_slugs
         return result
